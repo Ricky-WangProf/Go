ed9a91d4f93ee9acf07632980283a19f211d6e3a
ricky-wangprof <ricky.wangprof@gmail.com>
2018-02-15 17:31:23 -0500

    card project updates

--- Cards/main.go
+++ Cards/main.go
@@ -1,10 +1,6 @@
 package main
 
 func main() {
-	cards := newDeck()
-
-	hand, remainingHands := deal(cards, 5)
-
-	hand.print()
-	remainingHands.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
